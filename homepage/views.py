dac094d8768af3cf8ad0b95aebf17a718954e300
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2023-03-18 08:10:06 -0600

    Homepage settings
    Now the homepage is the actual homepage. The directory setup is changed a little.

--- homepage/views.py
+++ homepage/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def HomeResponse(request) :
-    return render(request, 'homepage/index.html')
+    return render(request, 'index.html')
