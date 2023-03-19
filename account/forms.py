eb40c15e2bff601d94c5e33770b0effa6fab9593
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2023-03-18 23:55:19 -0600

    Added more information to user model

--- account/forms.py
+++ account/forms.py
@@ -1,12 +1,10 @@
-# accounts/forms.py
-
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+    email = forms.EmailField(max_length=255, help_text='Required. Enter a valid email address.')
 
     class Meta:
         model = CustomUser
-        fields = ('email', 'password1', 'password2')
+        fields = ('email', 'username', 'password1', 'password2', 'birthday')
--- account/models.py
+++ account/models.py
@@ -1,36 +1,51 @@
 # accounts/models.py
+import datetime
 
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, email, username, password, birthday, **extra_fields):
         if not email:
-            raise ValueError(_('The Email field must be set'))
+            raise ValueError('The Email field must be set')
+        if not username:
+            raise ValueError('The username field must be set')
+        extra_fields.setdefault('date_joined', datetime.datetime.now())
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        username = username.lower()
+
+        user = self.model(email=email, username=username, birthday=birthday, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_moderator(self, email, password, **extra_fields):
+    def create_moderator(self, email, username, password, birthday, **extra_fields):
+        extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', False)
+        return self.create_user(email, username, password, birthday, **extra_fields)
 
-        return self.create_user(email, password, **extra_fields)
+    def create_superuser(self, email, username, password, birthday=None, **extra_fields):
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, birthday, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    is_active = models.BooleanField(_('active'), default=True)
-    is_staff = models.BooleanField(_('staff'), default=False)
+    email = models.EmailField(max_length=255, null=False, unique=True)
+    username = models.CharField(max_length=50, null=False, unique=True)
+    date_joined = models.DateTimeField(null=True)
+    birthday = models.DateField(null=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'birthday']
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
     def __str__(self):
-        return self.email
+        return self.username
