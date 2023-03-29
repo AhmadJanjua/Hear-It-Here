f6ed1a6033ebda510f81c7be1f210c2b1f4f1eb2
Ahmad <100788678+AhmadJanjua@users.noreply.github.com>
2023-03-28 23:18:42 -0600

    Auto select the email field
    Now defaults to selecting email instead of the username field.

--- account/forms.py
+++ account/forms.py
@@ -5,13 +5,18 @@ from .models import CustomUser, Profile
 
 # a form to ask for user information regarding
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=255, label='Email Address')
+    email = forms.EmailField(max_length=255, label='Email Address', widget=forms.EmailInput(attrs={'autofocus': True}))
     username = forms.CharField(max_length=50, label='Username')
     password2 = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
 
     class Meta:
         model = CustomUser
         fields = ('email', 'username', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs.update({'autofocus': True})
+        self.fields['username'].widget.attrs.update({'autofocus': False})
 
 
 # form to update the profile information
