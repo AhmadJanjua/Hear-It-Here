9f77acf965359b4737ded2ee8585d4e493a38d74
issam-akhtar <issam.akhtar@gmail.com>
2023-03-28 18:09:48 -0600

    add genre match functionality

--- static/code/card.js
+++ static/code/card.js
@@ -1,39 +1,14 @@
 'use strict';
 
-function navBar() {
-  let searchBtn = document.querySelector('.searchBtn');
-  let closeBtn = document.querySelector('.closeBtn');
-  let searchBox = document.querySelector('.searchBox');
-  let navlist = document.querySelector('.nav-list');
-  let menuToggle = document.querySelector('.menuToggle');
-  let header = document.querySelector('header');
 
-  searchBtn.onclick = function () {
-    searchBox.classList.add('active');
-    closeBtn.classList.add('active');
-    searchBtn.classList.add('active');
-    menuToggle.classList.add('hide');
-    header.classList.remove('open');
-  }
-
-  closeBtn.onclick = function () {
-    searchBox.classList.remove('active');
-    closeBtn.classList.remove('active');
-    searchBtn.classList.remove('active');
-    menuToggle.classList.remove('hide');
-  }
-
-  menuToggle.onclick = function () {
-    header.classList.toggle('open');
-    searchBox.classList.remove('active');
-    closeBtn.classList.remove('active');
-    searchBtn.classList.remove('active');
-  }
-}
 var tinderContainer = document.querySelector('.tinder');
 var allCards = document.querySelectorAll('.tinder--card');
 var nope = document.getElementById('nope');
 var love = document.getElementById('love');
+let likedCards = [];
+var sum = 0;
+
+
 
 function initCards(card, index) {
   var newCards = document.querySelectorAll('.tinder--card:not(.removed)');
@@ -92,6 +67,14 @@ const popup = document.getElementById('popup');
       var rotate = xMulti * yMulti;
 
       event.target.style.transform = 'translate(' + toX + 'px, ' + (toY + event.deltaY) + 'px) rotate(' + rotate + 'deg)';
+      if (event.deltaX > 0) {
+      // The card was swiped to the right
+        console.log('Liked');
+        likedCards.push(event.target.id);
+    } else {
+      // The card was swiped to the left
+      console.log('Disliked');
+    }
       initCards();
       var cards = document.querySelectorAll('.tinder--card:not(.removed)');
       // popup.style.opacity = '0';
@@ -115,21 +98,42 @@ function createButtonListener(love) {
     popup.style.opacity = '0';
     if (cards.length == 0) {
       // alert('There are no more cards left!');
+      //if the last element of liked cards is empty, remove it
+      if (likedCards[likedCards.length - 1] == "") {
+        likedCards.pop();
+      }
+
+      console.log(likedCards);
+      console.log("There are no more cards left!");
+      //get ascii sum of liked cards
+
+      console.log("the sum is:" + sum);
+      
+      createPopup();
+
       // create a popup to show the user that there are no more cards left
       popup.style.opacity = '1';
       //add transition to the popup
       popup.style.transition = 'opacity 3s ease-in-out';
       return false;
     }
-
+    for (var i = 0; i < likedCards.length; i++) {
+      for (var j = 0; j < likedCards[i].length; j++) {
+        sum += likedCards[i].charCodeAt(j);
+      }
+    }
+    console.log(sum);
     var card = cards[0];
 
     card.classList.add('removed');
 
     if (love) {
       card.style.transform = 'translate(' + moveOutWidth + 'px, -100px) rotate(-30deg)';
+      likedCards.push(card.id);
+      console.log('Liked');
     } else {
       card.style.transform = 'translate(-' + moveOutWidth + 'px, -100px) rotate(30deg)';
+      console.log('Disiked');
     }
 
     initCards();
@@ -145,3 +149,27 @@ var loveListener = createButtonListener(true);
 nope.addEventListener('click', nopeListener);
 love.addEventListener('click', loveListener);
 
+function createPopup(){
+  //create a popup with the ascii sum
+  const popup = document.getElementById('popup');
+  //Rap genre
+  if (likedCards.includes('nas') && likedCards.includes('kdot')) {
+    popup.querySelector('h2').innerHTML = 'Your Genre is: Rap';
+    // popup.querySelector('img').src = can add image
+    popup.querySelector('p').textContent = 'Rap music is a genre of popular music that originated in the United States in the 1970s. It consists of a stylized rhythmic music that commonly accompanies rapping, which is a rhythmic and rhyming speech that is chanted. You got this because you swiped right on Nas-illmatic and Kendrick Lamar-To Pimp a Butterfly';
+  }
+  //Quran genre
+  else if (likedCards.includes('quran')  ) {
+    popup.querySelector('h2').innerHTML = 'Your Genre is: Quran';
+    // popup.querySelector('img').src = can add image
+    popup.querySelector('p').textContent = 'MashAllah brother! The Quran is the central religious text of Islam, which Muslims believe to be a revelation from God. It is widely regarded as the finest piece of literature in the Arabic language. You got this because you swiped right on the Quran';
+  }
+  
+  if (sum == 1732) {
+    popup.querySelector('h2').innerHTML = 'You swept right on all the cards??';
+    // popup.querySelector('img').src = can add image
+    popup.querySelector('p').textContent = 'Guess you like all types of music!';
+  }
+ 
+
+}
